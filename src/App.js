1c49baabf48b7e118cf3c9590442ed1b0046b474
serii <seriiburduja@gmail.com>
2020-07-28 10:10:46 +0300

    lorem

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello world!!!</h1>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Hello world!!!</h1>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa facere illum molestias? Ab dignissimos doloremque ea, molestiae nam necessitatibus nesciunt, nisi pariatur perferendis quae quod repudiandae sed similique tempora voluptates?</p>
+		</div>
+	);
 }
 
 export default App;
