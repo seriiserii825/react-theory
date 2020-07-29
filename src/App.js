8463ac26eda0c87c664bc3b5d1ab810bd2ea2f40
serii <seriiburduja@gmail.com>
2020-07-29 10:02:21 +0300

    changeTitleHandler

--- src/App.js
+++ src/App.js
@@ -3,21 +3,40 @@ import './App.css';
 import './Car/Car';
 import Car from "./Car/Car";
 
-function App() {
-	const divStyle = {
-		textAlign: 'center',
+class App  extends React.Component{
+	constructor(props) {
+		super(props);
+		this.state = {
+			cars: [
+				{name: 'Ford', year: 2018},
+				{name: 'Audi', year: 2016},
+				{name: 'Nisan', year: 2014},
+			],
+			pageTitle: 'React components'
+		}
 	}
-	return (
-		<div style={divStyle}>
-			<h1>Hello world!!!</h1>
-			<Car name={'Audi'} year={2014} />
-			<Car name={'Ford'} year={2017} >
-				<p>Color: </p>
-			</Car>
-			<Car name={'Mazda'} year={2011}/>
-			<Car name={'Toyota'} year={2014}/>
-		</div>
-	);
+
+	changeTitleHandler = () => {
+		console.log('Click');
+	}
+
+	render() {
+		const divStyle = {
+			textAlign: 'center',
+		};
+
+		const cars = this.state.cars;
+
+		return (
+			<div style={divStyle}>
+				<h1>{this.state.pageTitle}</h1>
+				<button onClick={this.changeTitleHandler}>Change title</button>
+				<Car name={this.state.cars[0].name} year={this.state.cars[0].year}/>
+				<Car name={this.state.cars[1].name} year={this.state.cars[1].year}/>
+				<Car name={this.state.cars[2].name} year={this.state.cars[2].year}/>
+			</div>
+		);
+	}
 }
 
 export default App;
