64f34134551b8437e2618a0c09853633899a7f0b
serii <seriiburduja@gmail.com>
2020-07-30 15:31:01 +0300

    hoc

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,33 +1,10 @@
 import React from "react";
 import './Car.css';
-// function Car() {
-// 	return (
-// 		<div>This is a component</div>
-// 	);
-// }
-
-// const car = () => {
-// 	return (
-// 		<div>This is a component</div>
-// 	);
-// }
-
-// const car = () => (
-// <div>
-// 	This is a component
-// 	<strong>test</strong>
-// </div>);
-//
-// export default car;
-
+import withClass from "../hoc/withClass";
 
 class Car extends React.Component {
 
 	render() {
-
-		// if (Math.random() > 0.7) {
-		// 	throw new Error('Car random failed')
-		// }
 
 		let carsInput = ['input'];
 
@@ -38,35 +15,24 @@ class Car extends React.Component {
 		}
 
 		return (
-			<div className="Car">
+			<React.Fragment>
 				<h3>Car name: {this.props.name}</h3>
 				<p>Year: <strong>{this.props.year}</strong></p>
 				<input className={carsInput.join(' ')} type="text" onChange={this.props.onChangeName}
 					   value={this.props.name}/>
 				<button onClick={this.props.onDelete}>Delete</button>
-			</div>
+			</React.Fragment>
 		)
+		// return (
+		// 	<div className="Car">
+		// 		<h3>Car name: {this.props.name}</h3>
+		// 		<p>Year: <strong>{this.props.year}</strong></p>
+		// 		<input className={carsInput.join(' ')} type="text" onChange={this.props.onChangeName}
+		// 			   value={this.props.name}/>
+		// 		<button onClick={this.props.onDelete}>Delete</button>
+		// 	</div>
+		// )
 	}
 }
 
-export default Car;
-
-// export default props => {
-// 	let carsInput = ['input'];
-//
-// 	if(props.name !== ''){
-// 		carsInput.push('green');
-// 	}else{
-// 		carsInput.push('red');
-// 	}
-//
-//
-// 	return (
-// 		<div className="Car">
-// 			<h3>Car name: {props.name}</h3>
-// 			<p>Year: <strong>{props.year}</strong></p>
-// 			<input className={carsInput.join(' ')} type="text" onChange={props.onChangeName} value={props.name}/>
-// 			<button onClick={props.onDelete}>Delete</button>
-// 		</div>
-// 	)
-// };
+export default withClass(Car,'Car');
--- src/Counter/Counter.js
+++ src/Counter/Counter.js
@@ -7,9 +7,14 @@ export default class Counter extends React.Component {
 	}
 
 	addCounter = () => {
-		this.setState({
-			counter: this.state.counter + 1
+		this.setState(function (prevState) {
+			return {
+				counter: prevState.counter + 1
+			}
 		})
+		// this.setState({
+		// 	counter: this.state.counter + 1
+		// })
 	}
 
 
